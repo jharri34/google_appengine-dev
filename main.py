4f18ef37450253e901fae0c3c86e6f1b8a4039c0
jharri34 <jharri34@uab.edu>
2013-07-22 10:46:44 -0500

    google app engine shout tutorial and udacity homework 1

---
+++ main.py
@@ -0,0 +1,46 @@
+import wsgiref.handlers
+from google.appengine.ext import db
+from google.appengine.ext import webapp
+
+from google.appengine.ext. webapp import template
+
+class Shout(db.Model):
+	message = db.StringProperty(
+		required=True)
+	when = db.DateTimeProperty(
+		auto_now_add=True)
+	who = db.StringProperty()
+
+
+
+class MyHandler(webapp.RequestHandler):
+	def get(self):
+		shouts = db.GqlQuery(
+			'SELECT * FROM Shout'
+			)
+		values = {
+		'shouts':shouts
+		}
+
+		self.response.out.write(
+			str(template.render('main.html',
+								values)))
+
+	def post(self):
+		shout = Shout(message=self.request.get(
+			'message'),who=self.request.get(
+			'who'))
+		shout.put()
+		self.redirect("/")
+
+
+
+def main():
+	app =  webapp.WSGIApplication([(r'.*', MyHandler)], debug=True)
+	wsgiref.handlers.CGIHandler().run(app)
+
+if __name__ == "__main__":
+	main()
+
+# print
+# print 'Hello Udacity'
